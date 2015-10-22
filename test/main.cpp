6ebba02b2bf2b4536a7c96923f5b9137d5c57639
qyvlik <qyvlik@qq.com>
2015-10-22 22:47:06 +0800

    删除测试代码
    删除测试代码

--- test/main.cpp
+++ test/main.cpp
@@ -1,31 +1,10 @@
 #include <iostream>
 
-#include "test-object-call/myobject.h"
-
-#include "../src/core/bridge.h"
-
 using namespace std;
 using namespace soften;
 
 
 int main(int argc, char** argv)
 {
-    (void)argc;
-    (void)argv;
-    BaseTypeBridge<int> i0;
-    BaseTypeBridge<int> i1(new int(10));
-
-    i0.ASSIGN(&i1) ;
-
-    BaseTypeBridge<int> ui1(new int(10));
-    BaseTypeBridge<int> ui2(new int(1));
-    ui1.ASSIGN_BIT_OR(&ui2);
-
-    cout << ui1 << endl;
-
-    // cout << i0 << endl;
-    // cout << i0.EQ(&i1) << endl;
-
-    // return MyObject::main(argc, argv);
     return 0;
 }
